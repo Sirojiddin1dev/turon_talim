c330028c31631e407835b8d9c379b31b9b5dc983
mukhriddinuz <109203312+Muhriddinuz@users.noreply.github.com>
2025-10-21 18:41:21 +0500

    'Muxriddin'

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return f"<{self.__class__.__name__} ({self.id})>"
+    
+    
+    
